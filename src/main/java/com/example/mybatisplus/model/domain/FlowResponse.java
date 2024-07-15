3039b83af231ecea5648a0056779e5b431d4bc61
Xuezhihengg <1937135744@qq.com>
2024-07-15 11:42:55 +0800

    审批流程接口完成

---
+++ src/main/java/com/example/mybatisplus/model/domain/FlowResponse.java
@@ -0,0 +1,13 @@
+package com.example.mybatisplus.model.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class FlowItem {
+    private String username;
+    private String name;
+}
